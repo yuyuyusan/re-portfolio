130dfff658b585311377b052ae7a8c67410b232e
yuyuyusan <youzhiqichuan@gmail.com>
2023-08-07 19:04:08 +0900

    [New]

--- src/app/_components/WorksList/index.tsx
+++ src/app/_components/WorksList/index.tsx
@@ -1,50 +1,11 @@
-// import { WorksArticle } from '@/app/_libs/microcms/client';
-// import WorksListItem from '../WorksListItem';
-
-// type Props = {
-//   articles?: WorksArticle[];
-// };
-
-// export default function WorksList({ articles }: Props) {
-//   if (!articles) {
-//     return null;
-//   }
-//   if (articles.length === 0) {
-//     return <p>記事がありません。</p>;
-//   }
-//   return (
-//     <ul className="grid flex-wrap grid-cols-3 gap-x-7 gap-y-20 justify-center">
-//       {articles.map((article) => (
-//         <WorksListItem key={article.id} works={article} />
-//       ))}
-//     </ul>
-//   );
-// }
-
-
-
-'use client'
 import { WorksArticle } from '@/app/_libs/microcms/client';
 import WorksListItem from '../WorksListItem';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+
 type Props = {
   articles?: WorksArticle[];
 };
 
 export default function WorksList({ articles }: Props) {
-  const [ isSelect, setIsSelect ] = useState(false);
-  const router = useRouter();
-
-  function handleClickCard() {
-    try {
-      (document as any).startViewTransition(() => {
-        setIsSelect(true);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  }
   if (!articles) {
     return null;
   }
@@ -54,9 +15,48 @@ export default function WorksList({ articles }: Props) {
   return (
     <ul className="grid flex-wrap grid-cols-3 gap-x-7 gap-y-20 justify-center">
       {articles.map((article) => (
-        <WorksListItem key={article.id} works={article} onClick={ handleClickCard }
-        isSelect={ isSelect }/>
+        <WorksListItem key={article.id} works={article} />
       ))}
     </ul>
   );
 }
+
+
+
+// 'use client'
+// import { WorksArticle } from '@/app/_libs/microcms/client';
+// import WorksListItem from '../WorksListItem';
+// import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// type Props = {
+//   articles?: WorksArticle[];
+// };
+
+// export default function WorksList({ articles }: Props) {
+//   const [ isSelect, setIsSelect ] = useState(false);
+//   const router = useRouter();
+
+//   function handleClickCard() {
+//     try {
+//       (document as any).startViewTransition(() => {
+//         setIsSelect(true);
+//       });
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+//   if (!articles) {
+//     return null;
+//   }
+//   if (articles.length === 0) {
+//     return <p>記事がありません。</p>;
+//   }
+//   return (
+//     <ul className="grid flex-wrap grid-cols-3 gap-x-7 gap-y-20 justify-center">
+//       {articles.map((article) => (
+//         <WorksListItem key={article.id} works={article} onClick={ handleClickCard }
+//         isSelect={ isSelect }/>
+//       ))}
+//     </ul>
+//   );
+// }
