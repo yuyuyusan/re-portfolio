a5a42ef8a0db85b38b676d6094908d8f95ec3ff8
yuyuyusan <youzhiqichuan@gmail.com>
2023-08-12 22:41:59 +0900

    [delete] api

--- src/app/_libs/microcms/api.ts
+++ src/app/_libs/microcms/api.ts
@@ -14,15 +14,15 @@ export const client = createClient({
 });
 
 
-if (!process.env.MICROCMS_SERVICE_DOMAIN2) {
-  throw new Error('MICROCMS_SERVICE_DOMAIN2 is required');
-}
-if (!process.env.MICROCMS_API_KEY2) {
-  throw new Error('MICROCMS_API_KEY2 is required');
-}
+// if (!process.env.MICROCMS_SERVICE_DOMAIN2) {
+//   throw new Error('MICROCMS_SERVICE_DOMAIN2 is required');
+// }
+// if (!process.env.MICROCMS_API_KEY2) {
+//   throw new Error('MICROCMS_API_KEY2 is required');
+// }
 
-// API取得用のクライアントを作成
-export const client2 = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN2,
-  apiKey: process.env.MICROCMS_API_KEY2,
-});
+// // API取得用のクライアントを作成
+// export const client2 = createClient({
+//   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN2,
+//   apiKey: process.env.MICROCMS_API_KEY2,
+// });
