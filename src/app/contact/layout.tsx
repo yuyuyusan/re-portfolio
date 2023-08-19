3de0a782f72b5c635414231fad17b4271d2cb1a2
yuyuyusan <youzhiqichuan@gmail.com>
2023-08-19 22:37:43 +0900

    [add] contactディレクトリ追加

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Hero from '@/app/_components/Hero';
+import Header from '@/app/_components/Header';
+import Footer from '@/app/_components/Footer';
+import styles from './index.module.scss';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <main className={styles.blogMain}>
+        <Hero title="Preparation" sub="準備中" />
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import styles from './index.module.scss';
+import LinkButton from '@/app/_components/LinkButton';
+export const revalidate = 60;
+
+export default async function staticPage() {
+  return (
+    <>
+      <section className={`${styles.container}`}>
+        <p>in preparation. Please wait.</p>
+        <div className={styles.linkButton}>
+          <LinkButton title="Return" href="/" />
+        </div>
+      </section>
+    </>
+  );
+}
