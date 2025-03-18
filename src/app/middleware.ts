204448aa5bd15f05b8bd34fa3779f76384fb407d
yuyuyusan <youzhiqichuan@gmail.com>
2025-03-18 21:31:57 +0900

    Update middleware.ts

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -5,18 +5,49 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
+  console.log('ミドルウェア発動😃');
+
+  // BASIC認証が有効でない場合はスキップする
+  if (process.env.ENABLE_BASIC_AUTH !== 'true') {
+    return NextResponse.next();
+  }
+
+  // 環境変数の設定がない場合はスキップする
+  if (
+    process.env.BASIC_AUTH_USERNAME === undefined ||
+    process.env.BASIC_AUTH_PASSWORD === undefined
+  ) {
+    return NextResponse.next();
+  }
+
+  // BASIC認証のチェック
   const basicAuth = req.headers.get('authorization');
-  const url = req.nextUrl;
+  console.log('authorizationヘッダを確認😎', basicAuth);
 
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(authValue).split(':');
+    const [username, password] = Buffer.from(authValue, 'base64')
+      .toString()
+      .split(':');
+    console.log('認証情報確認😲', authValue, username, password);
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
+    if (
+      username === process.env.BASIC_AUTH_USERNAME &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      // BASIC認証に成功した場合、アクセスを許可する
       return NextResponse.next();
     }
   }
-  url.pathname = '/api/auth';
 
-  return NextResponse.rewrite(url);
+  // BASIC認証に失敗した場合、エラーを表示する
+  console.log('認証失敗🙃');
+  return NextResponse.json(
+    { error: 'Basic Auth Required' },
+    {
+      // eslint-disable-next-line quotes
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+      status: 401,
+    }
+  );
 }
