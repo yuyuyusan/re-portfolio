3b6f47580dd4cdf80a1193c191816f360c2486cd
yuyuyusan <youzhiqichuan@gmail.com>
2025-03-18 21:28:42 +0900

    Update middleware.ts

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,38 +1,22 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// ユーザー名とパスワード
-const USERNAME = 'admin';
-const PASSWORD = 'password';
-
-// 認証処理
-export function middleware(req: NextRequest) {
-  const authHeader = req.headers.get('authorization');
-
-  // 認証情報がない場合、認証ダイアログを表示
-  if (!authHeader) {
-    return new NextResponse('Unauthorized', {
-      status: 401,
-      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
-    });
-  }
-
-  // ベーシック認証の情報をチェック
-  const encodedCredentials = authHeader.split(' ')[1];
-  const decodedCredentials = atob(encodedCredentials); // デコード
-  const [inputUsername, inputPassword] = decodedCredentials.split(':');
-
-  if (inputUsername !== USERNAME || inputPassword !== PASSWORD) {
-    return new NextResponse('Unauthorized', {
-      status: 401,
-      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
-    });
-  }
-
-  return NextResponse.next();
-}
-
-// Middlewareを適用するパス
 export const config = {
-  matcher: '/:path*', // 全ページに適用
+  matcher: ['/:path*'],
 };
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === '4dmin' && pwd === 'testpwd123') {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = '/api/auth';
+
+  return NextResponse.rewrite(url);
+}
